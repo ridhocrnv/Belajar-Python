44cc5e4cd984cf323e13b3e48b6d0974439e212f
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-23 10:30:36 +0800

    feat: menambahkan beberapa jenis tipe data pada variabel.py

--- Belajar-Dasar-Python/variabel.py
+++ Belajar-Dasar-Python/variabel.py
@@ -1,4 +1,18 @@
+# Tipe data string
 nama_depan = "Ridho"
 nama_belakang = "Ahmad Irawan"
+
+# Tipe data integer
+tahun_lahir = 2005
 umur = 19
+
+# Tipe data float
 tinggi = 168.5
+
+# Tipe data boolean
+is_student = True
+
+# Menampilkan tipe data
+print(type(nama_depan))
+print(type(tahun_lahir))
+print(type(tinggi))
