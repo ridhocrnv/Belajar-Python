b03a6a51fc5962b8317c5dfafa6bf9eaae30f916
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-23 11:02:27 +0800

    feat: menambahkan data_type.py

---
+++ Belajar-Dasar-Python/data_type.py
@@ -0,0 +1,41 @@
+# Lists
+'''
+- Can contain multiple values
+- Ordered and changeable
+- Use [] to define a list'''
+
+daftarBuah = ["apel", "jeruk", "pisang", "mangga"]
+print(daftarBuah)
+print(type(daftarBuah))
+
+# Tuples
+'''
+- Can contain multiple values
+- Ordered but unchangeable
+- Use () to define a tuple'''
+
+daftarLaptop = ("Asus", "Lenovo", "HP", "Dell")
+print(daftarLaptop)
+print(type(daftarLaptop))
+
+# Sets
+'''
+- Can contain multiple values
+- Unordered and unindexed
+- Unchangeable but can add new items
+- Use {} to define a set'''
+
+daftarAngka = {1, 2, 3, 4, 5}
+print(daftarAngka)
+print(type(daftarAngka))
+
+
+# Dictionaries
+'''
+- Can contain multiple values in key-value pairs
+- Ordered and changeable
+- Use {} to define a dictionary'''
+
+daftarMahasiswa = {"nama": "John", "usia": 20, "jurusan": "Informatika"}
+print(daftarMahasiswa)
+print(type(daftarMahasiswa))
