f641bc8e1d431ce57c9fd38f01e538edbee36ba4
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-23 10:36:17 +0800

    feat: menampilkan nilai variabel

--- Belajar-Dasar-Python/variabel.py
+++ Belajar-Dasar-Python/variabel.py
@@ -16,3 +16,9 @@ is_student = True
 print(type(nama_depan))
 print(type(tahun_lahir))
 print(type(tinggi))
+
+# Menampilkan nilai variabel
+print("Nama lengkap:", nama_depan, nama_belakang)
+print("Tahun lahir:", tahun_lahir)
+print("Tinggi:", tinggi, "cm")
+print("Apakah saya seorang mahasiswa?", is_student)
